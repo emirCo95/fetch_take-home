057188ad46197c7a3232e71203fa3ad18f33596c
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-03-10 18:38:01 -0400

    update Login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,9 @@
+import LoginForm from '@/components/LoginForm';
+
 export default function Login() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <h1>Login</h1>
+      <LoginForm />
     </div>
   );
 }
---
+++ components/LoginForm.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+//base_url
+const base_url = 'https://frontend-take-home-service.fetch.com';
+
+export default function Login() {
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+  });
+
+  //   const router = useRouter();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+
+    console.log(formData);
+
+    setLoading(false);
+  };
+
+  return (
+    <Card className="w-full max-w-md mx-auto mt-10 p-6 shadow-lg">
+      <CardHeader>
+        <CardTitle>Login to Your Account</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <Label className="py-2" htmlFor="email">
+              Name
+            </Label>
+            <Input
+              id="name"
+              name="name"
+              type="name"
+              required
+              onChange={handleChange}
+            />
+          </div>
+
+          <div>
+            <Label className="py-2" htmlFor="password">
+              Email
+            </Label>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              required
+              onChange={handleChange}
+            />
+          </div>
+
+          <Button type="submit" className="w-full" disabled={loading}>
+            {loading ? 'Logging in...' : 'Login'}
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
