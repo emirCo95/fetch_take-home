32b5dd4e9a6cbf8d125f91578b26de067bfb4a28
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-03-11 10:19:26 -0400

    update search functionality

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from 'react';
 import { BreedFilterBox } from '@/components/BreedFilterBox';
 import axios from 'axios';
+import DogCard from '@/components/DogCard';
 
 //base_url
 const base_url = 'https://frontend-take-home-service.fetch.com';
@@ -13,70 +14,50 @@ interface SearchResults {
   total: number;
 }
 
+interface Dog {
+  id: string;
+  img: string;
+  name: string;
+  age: number;
+  zip_code: string;
+  breed: string;
+}
+
 export default function SearchDogs() {
   const [breeds, setBreeds] = useState<string[]>([]);
-  const [selectedBreed, setSelectedBreed] = useState('');
+  const [selectedBreed, setSelectedBreed] = useState<string>('');
   const [searchResults, setSearchResults] = useState<SearchResults>({
     next: '',
     resultIds: [],
     total: 0,
   });
+  const [dogs, setDogs] = useState<Dog[]>([]);
 
-  useEffect(() => {
-    fetchBreeds();
-  }, []);
-
-  useEffect(() => {
-    const fetchDogData = async () => {
-      const response = await axios(`${base_url}/dogs/search`, {
-        method: 'GET',
-        params: {
-          breeds: selectedBreed,
-          size: 5,
-        },
-        withCredentials: true,
-      });
-
-      if (response.data) {
-        setSearchResults(response.data);
-      }
-
-      return searchResults.resultIds;
-    };
-
-    const fetchDogs = async ({ dogIds }: { dogIds: string[] }) => {
-      const response = await axios.post(`${base_url}/dogs`, dogIds, {
-        withCredentials: true,
-      });
-
-      if (response.data) {
-        console.log(response.data);
-      }
-    };
-    fetchDogData().then((result) => {
-      console.log(result);
-      fetchDogs({ dogIds: result });
+  const fetchDogData = async () => {
+    const response = await axios(`${base_url}/dogs/search`, {
+      method: 'GET',
+      params: {
+        breeds: selectedBreed,
+        size: 5,
+      },
+      withCredentials: true,
     });
-  }, [selectedBreed]);
 
-  // useEffect(() => {
-  //   const fetchDogs = async () => {
-  //     const response = await axios.post(
-  //       `${base_url}/dogs`,
-  //       searchResults.resultIds,
-  //       {
-  //         withCredentials: true,
-  //       }
-  //     );
+    if (response.data) {
+      setSearchResults(response.data);
+    }
 
-  //     if (response.data) {
-  //       console.log(response.data);
-  //     }
-  //   };
+    return searchResults.resultIds;
+  };
 
-  //   fetchDogs();
-  // }, [selectedBreed]);
-
+  const fetchDogs = async ({ dogIds }: { dogIds: string[] }) => {
+    const response = await axios.post(`${base_url}/dogs`, dogIds, {
+      withCredentials: true,
+    });
+    if (response.data) {
+      setDogs(response.data);
+    }
+  };
   const fetchBreeds = async () => {
     const response = await axios(`${base_url}/dogs/breeds`, {
       method: 'GET',
@@ -92,9 +73,24 @@ export default function SearchDogs() {
     setSelectedBreed(breed);
   };
 
+  useEffect(() => {
+    fetchBreeds();
+  }, []);
+
+  useEffect(() => {
+    fetchDogData().then((result) => {
+      fetchDogs({ dogIds: result });
+    });
+  }, [selectedBreed]);
+
   return (
     <div className="h-screen flex justify-center items-start space-x-8 pt-24 dark:bg-gray-400">
       <BreedFilterBox breeds={breeds} onFilterByBreed={handleFilterByBreed} />
+      <main className="flex flex-wrap gap-6 p-6 justify-center">
+        {dogs.map((dog) => (
+          <DogCard key={dog.id} dog={dog} />
+        ))}
+      </main>
     </div>
   );
 }
